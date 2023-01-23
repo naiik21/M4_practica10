591f643323f9b735dca72436b980ed0775bbad14
rule2003 <rllamas22@jaumebalmes.net>
2023-01-23 20:04:05 +0100

    ejercicios de Rauk

---
+++ exercici2_A.py
@@ -0,0 +1,13 @@
+myDict = {}
+myDict["Hola"] = 10
+myDict["Mesa"] = 20
+myDict["silla"] = 12
+myDict["Armario"] = 5
+myDict["Lampara"] = 7
+myDict["Libro"] = 17
+
+print(len(myDict))
+print(myDict.values())
+print(myDict.popitem())
+
+
